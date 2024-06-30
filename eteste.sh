03517b9bf13feaa5238756cee778333d63276472
AlanT <alan.costa@edu.ufes.br>
2024-06-30 13:33:59 -0300

    feat: integration completed(match and search).

--- eteste.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#Script para fazer o teste do código em c direto do erlang
-make
-erlc ./src/gleamexnif.erl
-erl < input.txt
-rm -f gleamexnif.beam
--- src/gleamexnif.erl
+++ src/gleamexnif.erl
@@ -2,19 +2,21 @@
 
 -on_load(init/0).
 
--export([match/2]).
+-export([match/2, search/4]).
 
 init() ->
-    %Obtendo o caminho absoluto para gleamex_nif.so
-    AbsPath = filename:absname("_build/gleamex_nif"),
-    erlang:load_nif(AbsPath, 0).
+    erlang:load_nif("_build/gleamex_nif", 0).
 
 match_nif(_Pattern, _Subject) ->
+    erlang:nif_error(not_loaded).
+
+search_nif(_Pattern, _Subject, _First, _Last) ->
     erlang:nif_error(not_loaded).
 
 % Essa função converte as strings de formato binário para o formato compatível com c
 match(_Pattern, _Subject) ->
     match_nif(erlang:binary_to_list(_Pattern), erlang:binary_to_list(_Subject)).
 
-%search(_Pattern, _Subject) ->
-%    erlang:nif_error(not_loaded).
+% Essa função converte as strings de formato binário para o formato compatível com c
+search(_Pattern, _Subject, _First, _Last) ->
+    search_nif(erlang:binary_to_list(_Pattern), erlang:binary_to_list(_Subject), _First, _Last).
